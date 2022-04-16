70628cea70453d025edad5e1b91656289e4838f4
Ibrokhimjon0823 <ibrokhimjonmakhamadaliev@gmail.com>
2022-04-16 18:34:55 +0500

    added media

--- config/settings.py
+++ config/settings.py
@@ -166,3 +166,7 @@ AUTH_USER_MODEL = 'accounts.CustomUser'
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
 DEFAULT_FROM_EMAIL = 'admin@djangobookstore.com'
+
+MEDIA_URL = '/media/' # new
+MEDIA_ROOT = str(BASE_DIR.joinpath('media')) # new
+
