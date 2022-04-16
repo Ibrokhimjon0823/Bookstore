f0406f8e16266d6796ac2d8bab667911b8dfcd8e
Ibrokhimjon0823 <ibrokhimjonmakhamadaliev@gmail.com>
2022-04-17 01:09:02 +0500

    added manage.py

--- config/settings.py
+++ config/settings.py
@@ -129,16 +129,16 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'db',
-        'PORT': 5432
-    }
-    # "default": env.dj_db_url("DATABASE_URL",
-    #                          default="postgres://postgres@db/postgres")
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': 'postgres',
+    #     'USER': 'postgres',
+    #     'PASSWORD': 'postgres',
+    #     'HOST': 'db',
+    #     'PORT': 5432
+    # }
+    "default": env.dj_db_url("DATABASE_URL",
+                             default="postgres://postgres@db/postgres")
 }
 
 
